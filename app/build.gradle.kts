99ed04861100fc7d8d9dfd10982484eedc905a7f
linardos paschopoulos <paschopoulosl@gmail.com>
2023-11-11 12:41:17 +0200

    sdk changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 
 android {
     namespace = "com.example.myweatherapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.myweatherapp"
-        minSdk = 33
+        minSdk = 34
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
